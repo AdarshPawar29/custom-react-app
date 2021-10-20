b4d82841e712f1a9f22d4f0f9526de3560fcc5a5
AdarshPawar29 <Adarsh.11601516@lpu.in>
2021-10-20 16:11:31 +0530

    style issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 import { StoplightProject } from "@stoplight/elements-dev-portal";
-// import "@stoplight/elements-dev-portal/styles.min.css";
+import "@stoplight/elements-dev-portal/styles.min.css";
 
 render(
   <StoplightProject projectId="cHJqOjYyNTgw" />,
